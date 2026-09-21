411f3bb5c43731f85a6f19303a624c196adb7b45
Nadav Erell <nadaverell@gmail.com>
2026-09-22 02:13:32 +0300

    Preserve nil-safe Job ownership lookup through shared predicate

--- internal/server/jobset_resources.go
+++ internal/server/jobset_resources.go
@@ -41,7 +41,7 @@ type jobSetPod struct {
 func jobSetOwnedPods(root *unstructured.Unstructured, jobs []*batchv1.Job, pods []*corev1.Pod) []jobSetPod {
 	owned := make(map[types.UID]*batchv1.Job)
 	for _, job := range jobs {
-		if jobSetControls(root, job) && job.UID != "" {
+		if job != nil && jobSetControls(root, job) && job.UID != "" {
 			owned[job.UID] = job
 		}
 	}
