adea80d8c37af3cdba41ca88026fa9fc749f5877
Nadav Erell <nadaverell@gmail.com>
2026-09-23 18:44:24 +0300

    Keep discovery coverage helper beside capabilities handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -59,6 +59,7 @@ import (
 	"github.com/skyhook-io/radar/internal/upgrade"
 	"github.com/skyhook-io/radar/internal/version"
 	"github.com/skyhook-io/radar/pkg/argoapi"
+	"github.com/skyhook-io/radar/pkg/capacityapi"
 	"github.com/skyhook-io/radar/pkg/conditions"
 	"github.com/skyhook-io/radar/pkg/hpadiag"
 	"github.com/skyhook-io/radar/pkg/k8score"
@@ -1293,6 +1294,36 @@ func (s *Server) handleClusterInfo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s.writeJSON(w, info)
+}
+
+func resourceDiscoveryCoverage(discovery *k8s.ResourceDiscovery) *capacityapi.SourceCoverage {
+	coverage := capacityapi.NewSourceCoverage(capacityapi.CoverageUnavailable, capacityapi.CoverageScopeCluster)
+	coverage.ReasonCode = "not_initialized"
+	coverage.ImpactFields = []string{"apiResources"}
+	if discovery == nil {
+		return &coverage
+	}
+
+	stats := discovery.Stats()
+	if !stats.LastSuccessfulRefresh.IsZero() {
+		observedAt := stats.LastSuccessfulRefresh
+		coverage.ObservedAt = &observedAt
+	}
+	switch {
+	case stats.Stale || (stats.LastSuccessfulRefresh.IsZero() && stats.LastError != ""):
+		coverage.Status = capacityapi.CoverageError
+		coverage.ReasonCode = "discovery_refresh_failed"
+	case stats.Partial:
+		coverage.Status = capacityapi.CoveragePartial
+		coverage.ReasonCode = "partial_discovery"
+	case stats.LastSuccessfulRefresh.IsZero():
+		coverage.ReasonCode = "not_attempted"
+	default:
+		coverage.Status = capacityapi.CoverageAvailable
+		coverage.ReasonCode = ""
+		coverage.ImpactFields = []string{}
+	}
+	return &coverage
 }
 
 func (s *Server) handleCapabilities(w http.ResponseWriter, r *http.Request) {
